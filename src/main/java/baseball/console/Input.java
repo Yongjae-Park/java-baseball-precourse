c7178029da6a741a2e653652518a8754328b5444
yongjae_park <ytothej92@gmail.com>
2022-04-14 11:45:23 +0900

    feat(Output, Input) : Add message format

--- src/main/java/baseball/console/Input.java
+++ src/main/java/baseball/console/Input.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     private static final String INPUT_MESSAGE ="숫자를 입력해주세요 : ";
+    private static final String RESTART_OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static String getNumber() {
         System.out.print(INPUT_MESSAGE);
--- src/main/java/baseball/console/Output.java
+++ src/main/java/baseball/console/Output.java
@@ -3,6 +3,7 @@ package baseball.console;
 import baseball.domain.Rating;
 
 public class Output {
+    private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void printMessage(String message) {
         System.out.println(message);
