dee37fa73833c1ffc5ab3f54f49e46ae5d47103d
yongjae_park <ytothej92@gmail.com>
2022-04-15 12:02:40 +0900

    feat(Message, Input, Output) : Add Message enum
    Define enum 'Message'
    Remove message value static field

--- src/main/java/baseball/console/Input.java
+++ src/main/java/baseball/console/Input.java
@@ -1,23 +1,21 @@
 package baseball.console;
 
+import baseball.domain.Message;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
-
-    private static final String INPUT_MESSAGE ="숫자를 입력해주세요 : ";
-    private static final String RESTART_OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String RESTART = "1";
     private static final String TERMINATION = "2";
 
     public static String getNumber() {
-        System.out.print(INPUT_MESSAGE);
+        System.out.print(Message.INPUT_MESSAGE.getMessage());
         String inputNumber = Console.readLine();
         return inputNumber;
     }
 
     public static boolean isRestart(){
         boolean isRestart = false;
-        System.out.println(RESTART_OPTION_MESSAGE);
+        System.out.println(Message.RESTART_OPTION_MESSAGE.getMessage());
         String answer = Console.readLine();
         if(answer.equals(RESTART))
             isRestart = true;
--- src/main/java/baseball/console/Output.java
+++ src/main/java/baseball/console/Output.java
@@ -3,8 +3,6 @@ package baseball.console;
 import baseball.domain.Rating;
 
 public class Output {
-    public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    public static final String END_MESSAGE = "2를 선택하셨습니다. 진짜 종료";
     public static final int BALL = 0;
     public static final int STRIKE = 1;
 
