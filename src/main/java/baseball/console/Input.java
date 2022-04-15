02d3a7bd2fc4457fbf28e98d664db2b19521a025
yongjae_park <ytothej92@gmail.com>
2022-04-15 11:39:10 +0900

    feat(Input) : Add isRestart() for receiving restart option from user

--- src/main/java/baseball/console/Input.java
+++ src/main/java/baseball/console/Input.java
@@ -6,6 +6,8 @@ public class Input {
 
     private static final String INPUT_MESSAGE ="숫자를 입력해주세요 : ";
     private static final String RESTART_OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String RESTART = "1";
+    private static final String TERMINATION = "2";
 
     public static String getNumber() {
         System.out.print(INPUT_MESSAGE);
@@ -13,4 +15,12 @@ public class Input {
         return inputNumber;
     }
 
+    public static boolean isRestart(){
+        boolean isRestart = false;
+        System.out.println(RESTART_OPTION_MESSAGE);
+        String answer = Console.readLine();
+        if(answer.equals(RESTART))
+            isRestart = true;
+        return isRestart;
+    }
 }
