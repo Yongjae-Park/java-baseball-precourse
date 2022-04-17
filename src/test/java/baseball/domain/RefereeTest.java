1af6f495715f834c93d7923ce6a2e839097ac896
yongjae_park <ytothej92@gmail.com>
2022-04-17 14:00:41 +0900

    test(RefereeTest) : Add scoreRater method test

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,10 +1,20 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RefereeTest {
+
+    private Referee referee;
+
+    @BeforeEach
+    private void setUp() {
+        this.referee = new Referee();
+    }
 
     @Test
     public void 볼카운트_체크_확인() {
@@ -26,5 +36,12 @@ class RefereeTest {
         if(answer.indexOf(input)==index)
             isStrike = true;
         assertEquals(true,isStrike);
+    }
+
+    @Test
+    @DisplayName("scoreRater 메소드 테스트")
+    public void scoreRater_OneBallOneStrike_OneBallOneStrike() {
+        int[] scores = referee.scoreRater("123","253");
+        assertArrayEquals(new int[] {1,1},scores);
     }
 }
